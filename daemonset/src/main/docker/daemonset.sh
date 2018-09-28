5eed8f179b44ff366303558a9c5b357a72b9a30e
Wilson Mo <wilsonfv@126.com>
2018-09-28 16:12:52 +0800

    test binary authorization

---
+++ daemonset/src/main/docker/daemonset.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#rm -f /etc/resolv.conf
+#cat > /etc/resolv.conf << MYEOF
+#nameserver 10.70.200.44
+#nameserver 10.70.200.43
+#nameserver 10.71.200.43
+#nameserver 10.71.200.44
+#search cloud.uk.hsbc,hk.hsbc,uk.hsbc
+#MYEOF
+chmod 644 /etc/resolv.conf
